1ecd708f2954088f280b822eafe26900929528cc
angelinps.17 <angelinps.17@live.com>
2025-10-27 13:34:53 -0500

    update image name

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,17 +1,25 @@
 terraform {
   required_version = ">= 1.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "prod-state-bucketpsd"      # Nombre del bucket S3 donde guardarás el estado
+    key            = "terraform.tfstate" # Ruta dentro del bucket
+    region         = "us-east-1"                      # Región del bucket
+    dynamodb_table = "terraform-locks"                # (Opcional, para bloqueo del estado)
+    encrypt        = true                             # Encripta el archivo de estado en S3
+  }
 }
 
 provider "aws" {
   region = var.aws_region
 }
-
 # ECR Repository
 resource "aws_ecr_repository" "devops_microservice" {
   name = "devops-microservice"
@@ -25,7 +33,7 @@ resource "aws_ecr_repository" "devops_microservice" {
 resource "aws_eks_cluster" "devops_cluster" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster.arn
-  version  = "1.29"  # ← CAMBIA de 1.28 a 1.29
+  version  = "1.29"  #
 
   vpc_config {
     subnet_ids = [aws_subnet.public_1.id, aws_subnet.public_2.id]
